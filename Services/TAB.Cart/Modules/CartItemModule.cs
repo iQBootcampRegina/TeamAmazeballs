76ac60cdb6ae3640148864e1b3171265273328e3
Johannes Moersch <Johannes.Moersch@gmail.com>
2015-01-14 13:14:00 -0600

    Fixes missing files in cart service.

--- Services/TAB.Cart/Modules/CartItemModule.cs
+++ Services/TAB.Cart/Modules/CartItemModule.cs
@@ -8,5 +8,21 @@ namespace TAB.Cart.Modules
 {
 	public class CartItemModule : NancyModule
 	{
+		public CartItemModule()
+			: base("/Carts/{CartId}")
+		{
+			Get["/Items"] = x => GetItemsByCartId(x.CartId);
+			Get["/Items/{Id}"] = x => GetItemById(x.CartId, x.Id);
+		}
+
+		private object GetItemsByCartId(int cartId)
+		{
+			return "Heres your id " + cartId;
+		}
+
+		private object GetItemById(int cartId, int id)
+		{
+			return cartId + " | " + id;
+		}
 	}
 }
