8ff623083ef288378bec3021f6bbe57d3b53a35a
Brad Rhodes <bradr@iqmetrix.com>
2015-01-14 12:30:08 -0600

    Added models.

---
+++ Services/TAB.Cart/Models/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace TAB.Cart.Models
+{
+    public class Cart
+    {
+        public Cart()
+        {
+            Items = new List<Item>();
+        }
+
+        public int Id { get; set; }
+        public IList<Item> Items { get; set; }
+    }
+}
---
+++ Services/TAB.Cart/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿namespace TAB.Cart.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Sku { get; set; }
+    }
+}
