cfc0bb6a8bd6c3730213f2327d599ef6150a3195
Brad Rhodes <bradr@iqmetrix.com>
2015-01-14 12:52:23 -0600

    Added fake Product service.

--- Services/TAB.Cart/Services/IProductService.cs
+++ Services/TAB.Cart/Services/IProductService.cs
@@ -1,7 +1,36 @@
-﻿namespace TAB.Cart.Services
+﻿using System.Collections.Generic;
+using System.Linq;
+using Ploeh.AutoFixture;
+using TAB.Cart.Models;
+
+namespace TAB.Cart.Services
 {
     public interface IProductService
     {
-         
+        Product GetProductById(int id);
+    }
+
+    class FakeProductService : IProductService
+    {
+        private IList<Product> _products;
+
+        public FakeProductService()
+        {
+            _products = new List<Product>();
+            PopulateProducts();
+        }
+
+        public Product GetProductById(int id)
+        {
+            return _products.FirstOrDefault(x => x.Id == id);
+        }
+
+
+        private void PopulateProducts()
+        {
+            var fixture = new Fixture();
+
+            fixture.AddManyTo(_products, 100);
+        }
     }
 }
