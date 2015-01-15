480ba4bb9e7522c2c9afe76c37255c8e84122255
Brad Rhodes <bradr@iqmetrix.com>
2015-01-15 14:02:13 -0600

    Added necessary functionality to Order service.

---
+++ Services/TAB.Order.Webjob/IOrderRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TAB.Models.Order;
+
+namespace TAB.Order.Webjob
+{
+   public interface IOrderRepository
+    {
+        void CreateOrder(Models.Order.Order order);
+        Models.Order.Order UpdateToShippedStatus(Models.Order.OrderStatusUpdate status);
+    }
+
+    class OrderRepository : IOrderRepository
+    {
+        private IList<Models.Order.Order> _orders;
+
+        public OrderRepository()
+        {
+            _orders = new List<Models.Order.Order>();
+        }
+
+        public void CreateOrder(Models.Order.Order order)
+        {
+            _orders.Add(order);
+        }
+
+        public Models.Order.Order UpdateToShippedStatus(OrderStatusUpdate status)
+        {
+            if(!_orders.Any(x => x.Id == status.Id))
+                throw new ArgumentException("Order not found.");
+
+            _orders.First(x => x.Id == status.Id).OrderStatus = OrderStatus.Shipped;
+        }
+    }
+}
--- Services/TAB.Order.Webjob/Program.cs
+++ Services/TAB.Order.Webjob/Program.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IQ.Foundation.Messaging;
+using IQ.Foundation.Messaging.AzureServiceBus;
 using IQ.Foundation.Messaging.AzureServiceBus.Configuration;
+using IQ.Foundation.Messaging.MessageHandling;
+using TAB.Models.Order;
 
 namespace TAB.Order.Webjob
 {
@@ -11,6 +15,78 @@ namespace TAB.Order.Webjob
     {
         static void Main(string[] args)
         {
+            var bootstrapper = new DefaultAzureServiceBusBootstrapper(new OrderQueueConfiguration());
+
+            var orderRepo = new OrderRepository();
+            var notificationPublisher = bootstrapper.BuildMessagePublisher();
+            var orderHandler = new OrderHandler(orderRepo, notificationPublisher);
+            var orderStatusUpdateHandler = new OrderStatusUpdateHandler(orderRepo);
+
+            bootstrapper.MessageHandlerRegisterer.Register<Models.Order.Order>(orderHandler);
+            bootstrapper.MessageHandlerRegisterer.Register<Models.Order.OrderStatusUpdate>(orderStatusUpdateHandler);
+            bootstrapper.Subscribe();
+
+
+            Console.WriteLine("Waiting for orders...");
+            Console.ReadLine();
+        }
+    }
+
+    public class OrderStatusUpdateHandler : BaseMessageHandler<Models.Order.OrderStatusUpdate>
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderStatusUpdateHandler(IOrderRepository orderRepository)
+        {
+            if (orderRepository == null) throw new ArgumentNullException("orderRepository");
+            _orderRepository = orderRepository;
+        }
+
+        public override void Handle(OrderStatusUpdate message)
+        {
+            _orderRepository.UpdateToShippedStatus(message);
+        }
+    }
+
+    public class OrderHandler : BaseMessageHandler<Models.Order.Order>
+    {
+        private readonly IOrderRepository _orderRepo;
+        private readonly IPublishMessages _publisher;
+
+        public OrderHandler(IOrderRepository orderRepo, IPublishMessages publisher)
+        {
+            if (orderRepo == null) throw new ArgumentNullException("orderRepo");
+            _orderRepo = orderRepo;
+            _publisher = publisher;
+        }
+
+        public override void Handle(Models.Order.Order message)
+        {
+            _orderRepo.CreateOrder(message);
+
+            _publisher.Publish(message);
+        }
+    }
+
+    class OrderNotificationsConfiguration : ConventionServiceBusConfiguration
+    {
+        public override string ConnectionString
+        {
+            get
+            {
+                return
+                    "Endpoint=sb://iq-azgddlocal.servicebus.windows.net/;SharedSecretIssuer=owner;SharedSecretValue=+kXqZEalbQwKlsUq9B9NzA2TYX2JUorpOXSOAZIsGAc=";
+            }
+        }
+
+        public override string ServiceIdentifier
+        {
+            get { return "tab.order.notifications"; }
+        }
+
+        protected override bool PublishesMessages
+        {
+            get { return true; }
         }
     }
 
@@ -20,14 +96,20 @@ namespace TAB.Order.Webjob
         {
             get
             {
-                throw new NotImplementedException();
+                return
+                    "Endpoint=sb://iq-azgddlocal.servicebus.windows.net/;SharedSecretIssuer=owner;SharedSecretValue=+kXqZEalbQwKlsUq9B9NzA2TYX2JUorpOXSOAZIsGAc=";
 
             }
         }
 
         public override string ServiceIdentifier
         {
-            get { throw new NotImplementedException(); }
+            get { return "tab.order.commands"; }
+        }
+
+        protected override bool ConsumesQueue
+        {
+            get { return true; }
         }
     }
 }
