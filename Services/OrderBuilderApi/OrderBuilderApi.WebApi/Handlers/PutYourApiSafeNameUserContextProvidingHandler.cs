553b4f30dd9661a5320f21ab3007f50927c45110
JustinKasbrick <justink@iqmetrix.com>
2015-01-15 11:18:05 -0600

    Added hypermedia solution for order builder

---
+++ Services/OrderBuilderApi/OrderBuilderApi.WebApi/Handlers/PutYourApiSafeNameUserContextProvidingHandler.cs
@@ -0,0 +1,24 @@
+﻿using IQ.Platform.Framework.WebApi.AspNet;
+using IQ.Platform.Framework.WebApi.AspNet.Handlers;
+using IQ.Platform.Framework.WebApi.Services.Security;
+using OrderBuilderApi.ApiServices.Security;
+
+namespace OrderBuilderApi.WebApi.Handlers
+{
+    public class PutYourApiSafeNameUserContextProvidingHandler
+            : ApiSecurityContextProvidingHandler<OrderBuilderApiApiUser, NullUserContext>
+    {
+
+        public PutYourApiSafeNameUserContextProvidingHandler(
+            IStoreDataInHttpRequest<OrderBuilderApiApiUser> apiUserInRequestStore)
+            : base(new OrderBuilderApiUserFactory(), CreateContextProvider(), apiUserInRequestStore)
+        {
+        }
+
+        static ApiUserContextProvider<OrderBuilderApiApiUser, NullUserContext> CreateContextProvider()
+        {
+            return
+                new ApiUserContextProvider<OrderBuilderApiApiUser, NullUserContext>(_ => new NullUserContext());
+        }
+    }
+}
---
+++ Services/OrderBuilderApi/OrderBuilderApi.WebApi/Infrastructure/CustomPingResourceFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using IQ.Platform.Framework.WebApi.Diagnostic;
+
+namespace OrderBuilderApi.WebApi.Infrastructure
+{
+    /// <summary>
+    /// A ping resource creator.
+    /// </summary>
+    class CustomPingResourceFactory : ICreatePingResource
+    {
+        public PingResource Create()
+        {
+
+            var version =
+                Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            //TODO: provide the API data store version
+            Debugger.Break();
+            //var dbVersion = 
+
+            return new PingResource(version, "");
+        }
+    }
+}
